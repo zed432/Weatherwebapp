465cc6e26cc89774f579bf285cb531fdcb8e2ac4
zed432 <70649559+zed432@users.noreply.github.com>
2021-09-27 11:36:32 +0100

    Add files via upload

---
+++ wheatherwebapp/scripts/app.js
@@ -0,0 +1,73 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+
+const updateUI = (data) => {
+
+    console.log(data);
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    //destructure properties
+    const { cityDets, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;c</span>
+    </div>
+    `;
+
+    //update the night/day & icon images
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    //ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+    time.setAttribute('src', timeSrc);
+
+    
+   
+    if (card.classList.contains('d-none'))
+    {
+        card.classList.remove('d-none');
+        }
+};
+
+
+cityForm.addEventListener('submit', e => {
+    // prevent default action
+    e.preventDefault();
+
+    // get any city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the ui with new city
+    forecast.updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+    
+    
+    // set local storage 
+localStorage.setItem('city', city);
+});
+
+
+if (localStorage.getItem('city')) {
+    forecast.updateCity(localStorage.getItem('city'))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+}
+
+
+ 
---
+++ wheatherwebapp/scripts/forecast.js
@@ -0,0 +1,34 @@
+class Forecast{
+    constructor() {
+        this.key = 'Pbby56vVVFrXuqAMGaPSzsOOdYWPahN3';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return {cityDets, weather };
+    }
+    async getCity(city) {        
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id) {
+        
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
+
+
+
+// getCity('manchester').then(data => {
+//     return getWeather(data.Key);
+// }).then(data=> {
+//     console.log(data);
+//     }).catch(err => console.log(err)); 
